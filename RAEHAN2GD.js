0ee9a29b0b072ce776888877ca621af20110c69a
RAEHAN-SP <hanz.26r@gmail.com>
2026-09-04 00:27:45 +0800

    Update RAEHAN2GD.js

--- RAEHAN2GD.js
+++ RAEHAN2GD.js
@@ -416,31 +416,23 @@ break;
 			break;
 
 			case 's': {
-                                if (!/image|video|sticker/.test(quoted.type) && !/
-image|video|webp/.test(mime)) {
-                                        return m.reply(`Kirim/reply gambar/video/g
-if dengan caption *${prefix + command}*`);
-                                }
-                                m.react('⏳');
-                                let media = await RAEHAN2GD.downloadAndSaveMediaMe
-ssage(qmsg);
-                                let teks1 = text.split`|`[0] ? text.split`|`[0].tr
-im() : packname;
-                                let teks2 = text.split`|`[1] ? text.split`|`[1].tr
-im() : author;
+    if (!/image|video|sticker/.test(quoted.type) && !/image|video|webp/.test(mime)) {
+        return m.reply(`Kirim/reply gambar/video/gif dengan caption *${prefix + command}*`);
+    }
+    m.react('⏳');
+    let media = await RAEHAN2GD.downloadAndSaveMediaMessage(qmsg);
+    let teks1 = text.split`|`[0] ? text.split`|`[0].trim() : packname;
+    let teks2 = text.split`|`[1] ? text.split`|`[1].trim() : author;
 
-                                try {
-                                        await RAEHAN2GD.sendAsSticker(m.chat, medi
-a, m, { packname: teks1, author: teks2 });
-                                } catch (e) {
-                                        console.error("Gagal membuat stiker:", e);
-
-                                } finally {
-                                        if (fs.existsSync(media)) fs.unlinkSync(me
-dia);
-                                }
-                        }
-                        break;
+    try {
+        await RAEHAN2GD.sendAsSticker(m.chat, media, m, { packname: teks1, author: teks2 });
+    } catch (e) {
+        console.error("Gagal membuat stiker:", e);
+    } finally {
+        if (fs.existsSync(media)) fs.unlinkSync(media);
+    }
+}
+break;
 			
 			
 			
